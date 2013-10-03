295f19bdce4ede96a3261ba14790b5aef22627c9
kurtov <kurtov@unact.ru>
2013-10-03 20:05:36 +0400

    rails new shig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shig::Application.config.secret_key_base = 'db24b8ac0ab29dae39154e4455a6b2ec3b1fa25d660d6f393d16dc9d2aaebcda132c9e023ec85e2a16a58d07c9e6741317ab4c05e6a8f0cfaa07e200777b9780'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shig::Application.config.session_store :cookie_store, key: '_shig_session'
