ad5a3c476094750b49bef14db786b1025030b7a3
kurtov <kurtov@unact.ru>
2013-10-15 22:36:37 +0400

    twitt has many people

--- app/models/twitt.rb
+++ app/models/twitt.rb
@@ -14,4 +14,22 @@ class Twitt < ActiveRecord::Base
   def text_to_ary
     text.mb_chars.upcase.to_s.gsub(/Ё/, "Ё" => "Е").split.sort
   end
+  
+  def first_people
+    Person.joins(:twitt_person).where(:twitt_people => {:first => 1, :twitt_id => self.id})
+  end
+  
+  def first_people_to_s
+    p = first_people
+    
+    if p.size == 1
+      return "Да"
+    else
+      if p.size > 1
+        return "Да (#{p.map{|person| person.name}.join('; ')})" 
+      else
+        nil
+      end
+    end
+  end
 end
